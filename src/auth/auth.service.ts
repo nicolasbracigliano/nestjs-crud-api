f95172f93566a27592652cd59368e5123fa70d06
Nicolas Bracigliano <bracigliano@gmail.com>
2022-06-28 14:57:45 +1000

    feat: add auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
-@Injectable()
-export class AuthService {}
+@Injectable({})
+export class AuthService {
+  signup() {
+    return { msg: "I have signed up" };
+  }
+
+  signin() {
+    return { msg: "I have signed in" };
+  }
+}
